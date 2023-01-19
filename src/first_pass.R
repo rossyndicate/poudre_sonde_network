f19aafe420a789bf972c6de940a00b8fcb60e786
kathryn-willi <kathryn.willi@colostate.edu>
2023-01-19 10:25:03 -0700

    Updated notes/site meta data

--- src/first_pass.R
+++ src/first_pass.R
@@ -3,16 +3,16 @@
 first_pass <- function(what, parama, max = 100000, min = -100000){
 
   it <- what %>%
-    dplyr::select("DT", "DT_instrument", parama) %>%
-    rename(raw=3) %>%
+    dplyr::select("DT", parama) %>%
+    rename(raw = 2) %>%
     arrange(ymd_hms(DT)) %>%
     # Pass one (p1): remove any data outside what appears to be the natural range:
     mutate(p1 = ifelse(raw >= max, NA,
                 ifelse(raw <= min , NA, raw)),
            removed_flag = ifelse(p1 != raw, 1, 0)) %>%
     # For sites with more than one measurement within the 15-minute increment, average it
-    # and preserve how many data points are contained within that increment (n) the difference
-    # between the lowest and greatest data points.
+    # and preserve how many data points are contained within that increment (n_obs), and
+    # the difference between the lowest and greatest data points.
     group_by(DT) %>%
     summarize(p1 = as.numeric(mean(p1, na.rm = T)),
               diff = abs(min(p1, na.rm = T) - max(p1, na.rm = T)),
@@ -20,12 +20,12 @@ first_pass <- function(what, parama, max = 100000, min = -100000){
               removed_flag = sum(removed_flag)) %>%
   ungroup() %>%
    mutate(
-     front1=lead(p1, n=1),
-     front2=lead(p1, n=2),
-     front3=lead(p1, n=3),
-     back1=lag(p1, n=1),
-     back2=lag(p1, n=2),
-     back3=lag(p1, n=3)) %>%
+     front1=lead(p1, n = 1),
+     front2=lead(p1, n = 2),
+     front3=lead(p1, n = 3),
+     back1=lag(p1, n = 1),
+     back2=lag(p1, n = 2),
+     back3=lag(p1, n = 3)) %>%
    group_by(DT) %>%
    mutate(rollsd = sd(unlist(select(cur_data(), front1:back3))),
           rollavg=mean(unlist(select(cur_data(), front1:back3)))) %>%
