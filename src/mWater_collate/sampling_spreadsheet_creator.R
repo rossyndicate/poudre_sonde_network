de5d7c4b63a2854f5bad9953ba765d5d085af6b0
SamStruthers <sam.struthers2000@gmail.com>
2023-10-19 17:39:57 -0600

    moved all water sampling info to this function

--- src/mWater_collate/sampling_spreadsheet_creator.R
+++ src/mWater_collate/sampling_spreadsheet_creator.R
@@ -1,21 +1,60 @@
-sampling_spreadsheet_creator <- function(date_oi = "2023-10-16" ){
+sampling_spreadsheet_creator <- function(date_oi = "2023-10-16", all_dates = FALSE ){
 
   #pull in site meta data
   site_meta <- read_csv("data/water_sampling_sites.csv")%>%
     select(site = site_code, Site_Name, site_label_rmrs)
-#grab desired date
-  date_oi_clean <- as.Date(date_oi)
-# filter sampling notes df by desired date
-  samples_of_day <- filter(sampling_notes,date == date_oi_clean )%>%
+
+  # create df of all water samples and save DT, handheld probe and chla volume data
+  sampling_notes <- all_notes_cleaned%>%
+    filter(grepl("Sampling",visit_type))%>%
+    mutate(all_pics_taken = case_when(!is.na(downstream_pic)&!is.na(upstream_pic)&!is.na(clarity)&!is.na(filter_pic) ~ TRUE, TRUE ~ FALSE))%>%
+    select(site,crew, DT_round, date, time = time_start, sample_collected, chla_volume_ml, vol_filtered_blank_dup, do_mgl, cond_ms_cm, temp_c, visit_comments, all_pics_taken)
+
+  # Distinguish BLANK and DUPLICATE values
+  blanks_dups <- sampling_notes %>%
+    #find all values that have blank or dup
+    filter(grepl("DUPLICATE|BLANK", sample_collected)) %>%
+    # change sample collected to match BLANK/DUP
+    mutate(sample_collected = ifelse(grepl("DUPLICATE", sample_collected), "DUPLICATE", "BLANK"),
+           # Volume filtered blank dup becomes chla volume
+           chla_volume_ml = vol_filtered_blank_dup,
+           #drop vol_filtered_blank/dup
+           vol_filtered_blank_dup = NULL)
+
+  # Add blank and duplicate values back to main
+  sampling_notes <- sampling_notes%>%
+    #get rid of blank/dup in sample collcected
+    mutate(sample_collected = gsub("DUPLICATE|BLANK| |,", "", sample_collected),
+           #drop vol_filtered_blank/dup
+           vol_filtered_blank_dup = NULL)%>%
+    #bring in blank and dup rows
+    rbind(blanks_dups)%>%
+    #arrange by datetime and site (Blanks and dups go second)
+    arrange(DT_round, site)%>%
     # join with RMRS friendly metadata
-    left_join(site_meta, by = "site")%>%
-    #match date to RMRS style
-    mutate(Date = format(date, "%d-%b-%y"),
-           #create SiteDescr column for RMRS sheet
-           SiteDescr = paste0(site, "_",format(date, "%m%d%y")))%>%
-    # select only the needed columns, saved in the correct order and fix column names
-    select(Site = site_label_rmrs ,SiteDescr, SiteLabel = site, Date, SampleType = sample_collected,
-           time, do_mgl, cond_ms_cm, temp_c, notes = visit_comments  )
-# write to csv
-  write_csv(x = samples_of_day, file = paste0("data/sampling_notes/samples_of_",date_oi,".csv" ))
+    left_join(site_meta, by = "site")
+
+
+
+ if (all_dates == TRUE) {
+
+   # write to csv
+   write_csv(x = sampling_notes, file = paste0("data/sampling_notes/all_samples_as_of_",as.character(Sys.Date()),".csv" ))
+ }else{
+   #grab desired date
+   date_oi_clean <- as.Date(date_oi)
+   # filter sampling notes df by desired date
+   samples_of_day <- filter(sampling_notes,date == date_oi_clean )%>%
+     #match date to RMRS style
+     mutate(Date = format(date, "%d-%b-%y"),
+            #create SiteDescr column for RMRS sheet
+            SiteDescr = paste0(site, "_",format(date, "%m%d%y")))%>%
+     # select only the needed columns, saved in the correct order and fix column names
+     select(Site = site_label_rmrs ,SiteDescr, SiteLabel = site, Date, SampleType = sample_collected,
+            time, do_mgl, cond_ms_cm, temp_c, notes = visit_comments  )
+   # write to csv
+   write_csv(x = samples_of_day, file = paste0("data/sampling_notes/samples_of_",date_oi,".csv" ))
+ }
 }
+
+
