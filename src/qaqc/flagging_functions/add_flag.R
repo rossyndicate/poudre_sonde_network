6ee60650c5518ccdc627a9ad2edb0a45f56104c4
kathryn-willi <kathryn.willi@colostate.edu>
2023-10-25 16:23:26 -0600

    moved functions around

--- src/qaqc/flagging_functions/add_flag.R
+++
@@ -1,7 +0,0 @@
-# flag addition function
-add_flag <- function(df, condition_arg, description_arg) {
-  df <- df %>% mutate(flag = case_when(
-    {{condition_arg}} ~ if_else(is.na(flag), paste(description_arg), paste(flag, description_arg, sep = ";\n")),
-    TRUE ~ flag))
-  return(df)
-}
