d4c715a7d24d17338ed3e6feb7b8d3135cd1ddcf
kathryn-willi <kathryn.willi@colostate.edu>
2023-11-14 15:02:38 -0700

    now split slope by direction (positive vs. negative)

--- src/qaqc/download_and_flag_fxns/generate_summary_statistics.R
+++ src/qaqc/download_and_flag_fxns/generate_summary_statistics.R
@@ -32,7 +32,7 @@ generate_summary_statistics <- function(site_param_df) {
       rollsd = roll_sd(mean, n = 7, align = 'center', na.rm = F, fill = NA_real_),
       # Determine the slope of a point in relation to the point ahead and behind.
       slope_ahead = abs(front1 - mean)/15,
-      slope_behind = abs(mean - back1)/15,
+      slope_behind = (mean - back1)/15,
       rollslope = roll_mean(slope_behind, n = 7, align = 'center', na.rm = F, fill = NA_real_),
       # Add the standard deviation for points centered in a rolling slope of 7 points.
       # rollsdslope = roll_sd(slope_behind, n = 7, align = 'center', na.rm = F, fill = NA_real_),
@@ -41,33 +41,7 @@ generate_summary_statistics <- function(site_param_df) {
       month = month(DT_round),
       year = year(DT_round),
       y_m = paste(year, '-', month)) %>%
-    left_join(seasons, by = "month") %>%
-    group_by(season) %>%
-    # KW:: wondering if, there's a way to develop the sd using ONLY the range of data in
-    # the 10%-90% percentile range. Therefore, we don't include any of the weird outliers
-    # when trying to flag this way...
-    mutate(
-      # Seasonal 1st/99th percentile means and slope
-      m_mean01 = quantile(mean, 0.01, na.rm = TRUE),
-      m_mean99 = quantile(mean, 0.99, na.rm = TRUE),
-      m_slope_behind_01 = quantile(slope_behind, 0.01, na.rm = TRUE),
-      m_slope_behind_99 = quantile(slope_behind, 0.99, na.rm = TRUE)) %>%
-    ungroup() %>%
-    # Using ONLY data within the seasonal 1-99th percentile, get the standard deviation. Done to reduce
-    # noise from the major outliers of the dataset...
-    left_join(., site_param_df %>%
-                mutate(month = month(DT_round),
-                       year = year(DT_round),
-                       y_m = paste(year, '-', month)) %>%
-                left_join(seasons, by = "month") %>%
-                group_by(season) %>%
-                mutate(p01 = quantile(mean, 0.01, na.rm = TRUE),
-                       p99 = quantile(mean, 0.99, na.rm = TRUE)) %>%
-                filter(mean > p01 & mean < p99) %>%
-                # need to pull some of this info out and use it for the range limits to test it
-                summarize(m_sd_0199 = sd(mean, na.rm = T)) %>%
-                select(season, m_sd_0199),
-              by = "season")
+    left_join(seasons, by = "month")
 
   return(summary_stats_df)
 
