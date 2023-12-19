32c7885629796ce4ddcddfe43372ddfe600b8678
kathryn-willi <kathryn.willi@colostate.edu>
2023-12-19 17:23:16 -0500

    moving towards targets syntax

--- run.R
+++ run.R
@@ -1,3 +1,35 @@
+devtools::install_github("steeleb/HydroVuR")
+
+# list of packages required for this pipeline
+required_pkgs <- c("data.table",
+                   "httr2",
+                   "tidyverse",
+                   "rvest",
+                   "readxl",
+                   "zoo",
+                   "padr",
+                   "plotly",
+                   "feather",
+                   "RcppRoll",
+                   "yaml",
+                   "ggpubr",
+                   "profvis",
+                   "janitor")
+
+# helper function to install all necessary pacakges
+package_installer <- function(x) {
+  if (x %in% installed.packages()) {
+    print(paste0("{", x ,"} package is already installed."))
+  } else {
+    install.packages(x)
+    print(paste0("{", x ,"} package has been installed."))
+  }
+}
+
+# map function using base lapply
+lapply(required_pkgs, package_installer)
+
+
 #!/usr/bin/env Rscript
 
 # This is a helper script to run the pipeline.
