a4267fe0febad249017d20f03e85572033126bbc
kathryn-willi <kathryn.willi@colostate.edu>
2023-12-19 17:30:37 -0500

    added code to not add flags to historical data if it is already listed

--- src/qaqc/download_and_flag_fxns/add_seasonal_flag.R
+++ src/qaqc/download_and_flag_fxns/add_seasonal_flag.R
@@ -17,24 +17,24 @@ add_seasonal_flag <- function(df) {
     # Using seasonal cut-offs...
     left_join(lookup, by = "season") %>%
     # coalesce (t_mean01, t_mean99, t_slope_behind_01, t_slope_behind_99, t_sd_0199)
-    mutate(t_mean01 = coalesce(t_mean01.x, t_mean01.y),
-          t_mean99 = coalesce(t_mean99.x, t_mean99.y),
-          t_slope_behind_01 = coalesce(t_slope_behind_01.x, t_slope_behind_01.y),
-          t_slope_behind_99 = coalesce(t_slope_behind_99.x, t_slope_behind_99.y),
-          t_sd_0199 = coalesce(t_sd_0199.x, t_sd_0199.y)) %>%
-    select(!c(t_mean01.x, t_mean01.y,
-              t_mean99.x, t_mean99.y,
-              t_slope_behind_01.x, t_slope_behind_01.y,
-              t_slope_behind_99.x, t_slope_behind_99.y,
-              t_sd_0199.x, t_sd_0199.y)) %>%
-    relocate(c(historical_data, over_50_percent_fail_window, cleaner_flag), .after = "t_sd_0199") %>% # to do (j): i don't love this solution
+    # mutate(t_mean01 = coalesce(t_mean01.x, t_mean01.y),
+    #       t_mean99 = coalesce(t_mean99.x, t_mean99.y),
+    #       t_slope_behind_01 = coalesce(t_slope_behind_01.x, t_slope_behind_01.y),
+    #       t_slope_behind_99 = coalesce(t_slope_behind_99.x, t_slope_behind_99.y),
+    #       t_sd_0199 = coalesce(t_sd_0199.x, t_sd_0199.y)) %>%
+    # select(!c(t_mean01.x, t_mean01.y,
+    #           t_mean99.x, t_mean99.y,
+    #           t_slope_behind_01.x, t_slope_behind_01.y,
+    #           t_slope_behind_99.x, t_slope_behind_99.y,
+    #           t_sd_0199.x, t_sd_0199.y)) %>%
+    #relocate(c(historical, over_50_percent_fail_window), .after = "t_sd_0199") %>% # to do (j): i don't love this solution
     # ... flag obs that are outside the seasonal 1st - 99th percentile range:
-    add_flag((mean < t_mean01 | mean > t_mean99), "outside of seasonal range") %>%
+    add_flag(((mean < t_mean01 | mean > t_mean99) & !grepl("outside of seasonal range", flag)), "outside of seasonal range") %>%
     # flag obs whose slope is outside the 1st - 99th percentile range
-    add_flag((slope_ahead >= t_slope_behind_99 | slope_behind >= t_slope_behind_99 |
-              slope_ahead <= t_slope_behind_01 | slope_behind <= t_slope_behind_01), "slope violation") %>%
+    add_flag(((slope_ahead >= t_slope_behind_99 | slope_behind >= t_slope_behind_99 |
+              slope_ahead <= t_slope_behind_01 | slope_behind <= t_slope_behind_01) & !grepl("slope violation", flag)), "slope violation") %>%
     # ... flag obs that are outside of the rolling mean times 3 sd's of the 1-99 percentile seasonal values
-    add_flag((mean <= rollavg - (3 * as.numeric(t_sd_0199)) | mean >= rollavg + (3 * as.numeric(t_sd_0199))), "outside sd range") %>%
+    add_flag(((mean <= rollavg - (3 * as.numeric(t_sd_0199)) | mean >= rollavg + (3 * as.numeric(t_sd_0199))) & !grepl("outside sd range", flag)), "outside sd range") %>%
     # ... flag obs that are outside of the rolling slope... how to play around with this better....
     # add_flag((parameter == "Turbidity") &
     #            (slope_behind <= rollslope - (3 * rollsdslope) | slope_behind >= rollslope + (3 * rollsdslope)), "outside sd range") %>%
--- src/qaqc/download_and_flag_fxns/add_suspect_flag.R
+++ src/qaqc/download_and_flag_fxns/add_suspect_flag.R
@@ -32,14 +32,14 @@ add_suspect_flag <- function(df) {
 
   # Define a function to check if a given 6-hour window has >= 50% fails
   check_3_hour_window_fail <- function(x) {
-    sum(x) / length(x) >= 0.4
+    sum(x) / length(x) >= 0.5
   }
 
   df_test <- df %>%
     mutate(flag_binary = ifelse((is.na(flag) | grepl(flag_string, flag)), 0, 1)) %>%
     #arrange(timestamp) %>%
-    mutate(over_50_percent_fail_window = zoo::rollapply(flag_binary, width = 12, FUN = check_3_hour_window_fail, fill = NA, align = "right")) %>%
-    add_flag(over_50_percent_fail_window == TRUE, "suspect data") # is this flagging the correct data?
+    mutate(over_50_percent_fail_window = ifelse(is.na(over_50_percent_fail_window), zoo::rollapply(flag_binary, width = 12, FUN = check_3_hour_window_fail, fill = NA, align = "right"), over_50_percent_fail_window)) %>%
+    add_flag(over_50_percent_fail_window == TRUE & !grepl("suspect data", flag), "suspect data") # is this flagging the correct data?
 
 return(df_test)
 
