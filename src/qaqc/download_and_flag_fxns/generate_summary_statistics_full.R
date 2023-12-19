58bae46488f90f89b6248344782a7d894e0efeb5
kathryn-willi <kathryn.willi@colostate.edu>
2023-12-19 17:31:38 -0500

    made new functions for creating seasonal thresholds

---
+++ src/qaqc/download_and_flag_fxns/generate_summary_statistics_full.R
@@ -0,0 +1,54 @@
+# Generate summary statistics for a given site parameter data frame.
+#' @param site_param_df A data frame with a `mean` column retrieved from HydroVu API.
+#' @return A data frame with summary statistics for a given site parameter data frame.
+#' @examples
+# generate_summary_statistics(site_param_df = all_data_flagged$`archery-Actual Conductivity`)
+# generate_summary_statistics(site_param_df = all_data_flagged$`boxelder-Temperature`)
+
+generate_summary_statistics_full <- function(site_param_df) {
+
+  # # create seasons table
+  # winter_baseflow <- c(12,1,2,3,4)
+  # snowmelt <- c(5,6,NA,NA,NA)
+  # monsoon <- c(7,8,9,NA,NA)
+  # fall_baseflow <- c(10,11,NA,NA,NA)
+  #
+  # seasons <- cbind(winter_baseflow, snowmelt, monsoon, fall_baseflow) %>%
+  #   as_tibble() %>%
+  #   pivot_longer(cols = names(.), values_to = "month", names_to = "season") %>% drop_na()
+
+
+  summary_stats_df <- site_param_df %>%
+    # ... so that we can get the proper leading/lagging values across our entire timeseries:
+    mutate(
+      # Add the next value and previous value for mean.
+      front1 = lead(mean, n = 1), #ifelse(is.na(front1), lead(mean, n = 1), front1),
+      back1 = lag(mean, n = 1), #ifelse(is.na(back1), lag(mean, n = 1), back1),
+      # Add the median for a point centered in a rolling median of 7 points.
+      rollmed = roll_median(mean, n = 7, align = 'right', na.rm = F, fill = NA_real_), #ifelse(is.na(rollmed), roll_median(mean, n = 7, align = 'right', na.rm = F, fill = NA_real_), rollmed), # to go (j): check_na() function for when we append data
+      # Add the mean for a point centered in a rolling mean of 7 points.
+      rollavg = roll_mean(mean, n = 7, align = 'right', na.rm = F, fill = NA_real_), #ifelse(is.na(rollavg), roll_mean(mean, n = 7, align = 'right', na.rm = F, fill = NA_real_), rollavg),
+      # Add the standard deviation for a point centered in a rolling mean of 7 points.
+      rollsd = roll_sd(mean, n = 7, align = 'right', na.rm = F, fill = NA_real_), #ifelse(is.na(rollsd), roll_sd(mean, n = 7, align = 'right', na.rm = F, fill = NA_real_), rollsd),
+      # Determine the slope of a point in relation to the point ahead and behind.
+      # katie working on these (j)
+      slope_ahead = (front1 - mean)/15,
+      slope_behind = (mean - back1)/15,
+      rollslope = roll_mean(slope_behind, n = 7, align = 'right', na.rm = F, fill = NA_real_), #ifelse(is.na(rollslope), roll_mean(slope_behind, n = 7, align = 'right', na.rm = F, fill = NA_real_), rollslope),
+      # Add the standard deviation for points centered in a rolling slope of 7 points.
+      # rollsdslope = roll_sd(slope_behind, n = 7, align = 'right', na.rm = F, fill = NA_real_),
+      #KRW Comment: add monthly sd. add monthly avg. add 10th and 90th quantiles.
+      # add some summary info for future us
+      month = month(DT_round),
+      year = year(DT_round),
+      y_m = paste(year, '-', month),
+      season = case_when(month %in% c(12,1,2,3,4) ~ "winter_baseflow",
+                         month %in% c(5,6) ~ "snowmelt",
+                         month %in% c(7,8,9) ~ "monsoon",
+                         month %in% c(10,11) ~ "fall_baseflow",
+                         TRUE ~ NA)
+      )
+
+  return(summary_stats_df)
+
+}
---
+++ src/qaqc/download_and_flag_fxns/summarize_site_param_full.R
@@ -0,0 +1,83 @@
+# Summarize site parameter data from the API and field notes data frames.
+#' @param site_arg A site name.
+#' @param parameter_arg A parameter name.
+#' @param api_data A dataframe with the munged API data.
+#' @return A dataframe with summary statistics for a given site parameter data frame.
+#' @examples
+# summarize_site_param(site_arg = "archery", parameter_arg = "Actual Conductivity", api_data = api_data)
+# summarize_site_param(site_arg = "boxelder", parameter_arg = "Temperature", api_data = api_data)
+
+summarize_site_param_full <- function(site_arg, parameter_arg, api_data, notes = field_notes) {
+
+  # filter deployment records for the full join
+  site_field_notes <- notes %>%
+    filter(site == site_arg)
+
+  # filtering the data and generating results
+  summary <- tryCatch({
+    api_data %>%
+      # summarize the data
+      filter(site == site_arg & parameter == parameter_arg) %>%
+      distinct() %>%
+      group_by(DT_round, site, parameter) %>%
+      summarize(mean = as.numeric(mean(value, na.rm = T)),
+                diff = abs(min(value, na.rm = T) - max(value, na.rm = T)),
+                n_obs = n()) %>%
+      ungroup() %>%
+      arrange(DT_round) %>%
+      # pad the dataset so that all 15-min timestamps are present
+      pad(by = "DT_round", interval = "15 min") %>%
+      # join the field notes
+      mutate(DT_join = as.character(DT_round),
+             site = site_arg,
+             parameter = parameter_arg,
+             flag = NA) %>% # maybe we don't want to do this here
+      left_join(filter(dplyr::select(site_field_notes, sonde_employed, last_site_visit, DT_join, site, visit_comments, sensor_malfunction, cals_performed)),
+                by = c('DT_join', 'site')) %>%
+      mutate(DT_round = as_datetime(DT_join, tz = "MST")) %>%
+      # Use fill() to determine when sonde was in the field, and when the last site visit was.
+      tidyr::fill(c(sonde_employed, last_site_visit, sensor_malfunction)) #%>%
+      # format the data by adding all the columns that are missing from the flagged data
+      # mutate( # to do (j): reorganize the order of the columns
+      #   mean_public = NA,
+      #   front1 = NA,
+      #   back1 = NA,
+      #   rollmed = NA,
+      #   rollavg = NA,
+      #   rollsd = NA,
+      #   slope_ahead = NA,
+      #   slope_behind = NA,
+      #   rollslope = NA,
+      #   month = NA,
+      #   year = NA,
+      #   y_m = NA,
+      #   season = NA,
+      #   m_mean01 = NA,
+      #   m_mean99 = NA,
+      #   m_slope_behind_01 = NA,
+      #   m_slope_behind_99 = NA,
+      #   t_mean01 = NA,
+      #   t_mean99 = NA,
+      #   t_slope_behind_01 = NA,
+      #   t_slope_behind_99 = NA,
+      #   t_sd_0199 = NA,
+      #   historical_flagged_data_1 = FALSE,
+      #   historical_flagged_data_2 = NA,
+      #   over_50_percent_fail_window = NA,
+      #   cleaner_flag = NA # to do (j): rename
+      #   # verification = NA
+      #   # add column that signifies that this is new incoming data
+      # ) %>%
+      # relocate(mean_public, .after = "mean")
+  },
+
+  error = function(err) {
+    # error message
+    cat("An error occurred with site ", site_arg, " parameter ", parameter_arg, ".\n")
+    cat("Error message:", conditionMessage(err), "\n")
+    flush.console() # Immediately print the error messages
+    NULL  # Return NULL in case of an error
+  })
+
+  return(summary)
+}
