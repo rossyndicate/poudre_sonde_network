5cd69495922ae55ddb2daefdd0c02adc4e6a2a6d
juandlt-csu <juan.delatorre@colostate.edu>
2024-02-26 10:42:28 -0700

    Testing add_suspect_flag()

--- test_env/generate_subset_data.R
+++ test_env/generate_subset_data.R
@@ -3,6 +3,7 @@ library(targets)
 library(tarchetypes)
 library(tidyverse)
 library(yaml)
+library(zoo)
 
 # sourced functions ----
 qaqc_functions <- list.files(path = "src/qaqc/download_and_flag_fxns", pattern = "\\.R$", full.names = T)
@@ -34,12 +35,11 @@ test_all_data_flagged <- map(all_data_summary_stats_list, function(data){
     add_seasonal_flag() %>%
     add_na_flag() %>%
     add_repeat_flag()
-    # we should also be incorporating the add_malfuntion_flag() here no?
 })
 
 # Explore the outputs of test_all_data_flagged ----
 ### Remove the data from future metadata parameters (Baro, External Voltage, Battery, Depth?)
-metadata_params <- "Baro|Battery Level|External Voltage|Depth"
+metadata_params <- "Baro|Battery Level|External Voltage"
 test_all_data_flagged_filtered <- test_all_data_flagged %>% discard(.p = str_detect(.,metadata_params))
 
 ### Set seed to randomly test data from test_all_data_flagged_filtered
@@ -48,17 +48,36 @@ set.seed(123)
 # Start of pull ----
 ### select 3 random dfs from test_all_data_flagged_filtered list and take out what would
 ### be the equivalent of 6 hours of data from the top
-start_samples <- sample(test_all_data_flagged_filtered, 3)
-
-### filter out the data
-
+start_samples <- sample(test_all_data_flagged_filtered, 3) %>%
+  map(.x = ., ~{slice_head(.x, n = 24)})
 ### Write the data
+iwalk(.x = start_samples, function(data, idx) {
+  file_name <- paste0("test_env/test_data/", idx,"_head_6hr.csv")
+  write_csv(data, file_name)
+})
 
 ## End of pull ----
 ### select 3 random dfs from test_all_data_flagged_filtered list and take out what would
 ### be the equivalent of 6 hours of data from the bottom
-end_samples <- sample(test_all_data_flagged_filtered, 3)
+end_samples <- sample(test_all_data_flagged_filtered, 3) %>%
+  map(.x = ., ~{slice_tail(.x, n = 24)})
+### Write the data
+iwalk(.x = end_samples, function(data, idx) {
+  file_name <- paste0("test_env/test_data/", idx,"_tail_6hr.csv")
+  write_csv(data, file_name)
+})
 
 ## Random pulls ----
 ### select 3 random dfs from test_all_data_flagged_filtered list and take out what would
 ### be the equivalent of 6 hours of data randomly
+start_index_list <- sample(test_all_data_flagged_filtered, 3) %>%
+  map(.x = ., ~{sample(1:(nrow(.x)-23), 1)})
+
+random_samples <- sample(test_all_data_flagged_filtered, 3) %>%
+  map2(.x = ., .y = start_index_list, ~{slice(.x, .y:(.y+23))})
+
+### Write the data
+iwalk(.x = random_samples, function(data, idx) {
+  file_name <- paste0("test_env/test_data/", idx, "_rdm_6hr.csv")
+  write_csv(data, file_name)
+})
--- test_env/test-add_suspect_flag.R
+++ test_env/test-add_suspect_flag.R
@@ -7,6 +7,8 @@
 #' add_large_anomaly_flags(df = all_data_flagged$`archery-Actual Conductivity`)
 #' add_large_anomaly_flags(df = all_data_flagged$`boxelder-Temperature`)
 
+library(testthat)
+
 add_suspect_flag <- function(df) {
 
   flag_string <- "sonde not employed|missing data|site visit|sv window"
@@ -28,16 +30,229 @@ add_suspect_flag <- function(df) {
 
 }
 
-# List of subsets of data
+# Read in lists of subsets of data ----
+test_data_dir <- "test_env/test_data/"
 
+head_csv_files <- list.files(test_data_dir, pattern = "\\_head_6hr.csv$", full.names = T)
+head_data_list <- map(head_csv_files, read_csv)
 
-test_that(desc = "'suspect data' flag is not added to the first ", code = {
+tail_csv_files <- list.files(test_data_dir, pattern = "\\_tail_6hr.csv$", full.names = T)
+tail_data_list <- map(tail_csv_files, read_csv)
 
-  temp_C <- F_to_C(50); # Runs the function
+random_csv_files <- list.files(test_data_dir, pattern = "\\_rdm_6hr.csv$", full.names = T)
+random_data_list <- map(random_csv_files, read_csv)
 
-  # Test that the result is the correct value
-  expect_that( object = temp_C, condition = equals(10) );
+test_data_list <- list(head_data_list, tail_data_list, random_data_list)
+# Test the function ----
 
-  # Test that the result is numeric
-  expect_that( object = is.numeric(temp_C), condition = equals(TRUE) );
+## test on a single row of data
+
+### lists of samples
+single_row_list <- map(test_data_list,
+                       function(data) {
+                         map(data, ~{slice_sample(.x, n = 1)})
+                       })
+
+### test
+single_row_test <- function(row){
+  test_that(desc = "'suspect data' flag is not added to a single row",
+            code = {
+              test <- add_suspect_flag(row) # Runs the function
+              # Test that the object returned is a data frame
+              expect_true(is.data.frame(test))
+              # Test that the flag column does not contain 'suspect flag'
+              expect_no_match(test$flag, "suspect flag")
+              }
+            )
+}
+
+### test sample rows
+walk(single_row_list, function(data) {
+  walk(data, ~{single_row_test(.x)})
 })
+
+## test on data with 0% flags
+
+# I know that the dfs that I am about to test do not have any flags,
+# so we should not expect the "suspect flag" to be present in any of `flag`
+# columns after the `add_suspect_flag()` function is applied. (JD)
+
+### lists of samples
+df_no_flag_list <- map(test_data_list, ~.x[[1]])
+
+### test
+no_flag_test <- function(df) {
+  test_that(desc = "'suspect data' flag is not added to a df with no flags",
+            code = {
+              test <- add_suspect_flag(df)
+              expect_true(is.data.frame(test))
+              expect_no_match(test$flag, "suspect flag")
+              }
+            )
+}
+
+### test sample rows
+walk(df_no_flag_list, no_flag_test)
+
+## test synthetic data ----
+
+### test on data with flag string present ----
+flag_string <- c("sonde not employed","missing data","site visit","sv window")
+
+sv_data <- tibble(flag = rep(flag_string, times = 3), over_50_percent_fail_window = NA, .rows = 12)
+
+test_that("no flags add to data that contains only flag_string strings", code = {
+  test <- add_suspect_flag(sv_data)
+  expect_true(is.data.frame(test))
+  expect_no_match(test$flag, "suspect data")
+})
+
+sv_mix_data <- tibble(flag = rep(paste0(flag_string, ";\\n", rev(flag_string)), times = 3), over_50_percent_fail_window = NA, .rows = 12)
+
+test_that("no flags add to data that contains only flag_string strings", code = {
+  test <- add_suspect_flag(sv_mix_data)
+  expect_true(is.data.frame(test))
+  expect_no_match(test$flag, "suspect data")
+})
+
+### create single 3 hour chunk of data ----
+
+values <- c("sample flag", NA)
+
+# 0%
+leq50_0 <- tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 12)
+
+# 25%
+eq25_top <- bind_rows(tibble(flag = "sample flag", over_50_percent_fail_window = NA, .rows = 3),
+                       tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 9))
+
+eq25_bottom <- bind_rows(tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 9),
+                          tibble(flag = "sample flag", over_50_percent_fail_window = NA, .rows = 3))
+
+eq25_middle <- bind_rows(tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 4),
+                          tibble(flag = "sample flag", over_50_percent_fail_window = NA, .rows = 3),
+                          tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 5))
+
+eq25_alternate <- bind_rows(tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 3),
+                             tibble(flag = rep(values, times = 3), over_50_percent_fail_window = NA, .rows = 6),
+                             tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 6))
+
+eq25_list <- list(eq25_top,eq25_bottom,eq25_middle,eq25_alternate)
+
+# 50%
+eq50_top <- bind_rows(tibble(flag = "sample flag", over_50_percent_fail_window = NA, .rows = 6),
+                       tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 6))
+
+eq50_bottom <- bind_rows(tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 6),
+                          tibble(flag = "sample flag", over_50_percent_fail_window = NA, .rows = 6))
+
+eq50_middle <- bind_rows(tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 3),
+                          tibble(flag = "sample flag", over_50_percent_fail_window = NA, .rows = 6),
+                          tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 3))
+
+eq50_alternate <- bind_rows(tibble(flag = rep(values, times = 6), over_50_percent_fail_window = NA, .rows = 12))
+
+eq50_list <- list(eq50_top, eq50_bottom, eq50_middle, eq50_alternate)
+
+### test on data with 75% flags
+eq75_top <- bind_rows(tibble(flag = "sample flag", over_50_percent_fail_window = NA, .rows = 9),
+                       tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 3))
+
+eq75_bottom <- bind_rows(tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 3),
+                          tibble(flag = "sample flag", over_50_percent_fail_window = NA, .rows = 9))
+
+eq75_middle <- bind_rows(tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 1),
+                          tibble(flag = "sample flag", over_50_percent_fail_window = NA, .rows = 9),
+                          tibble(flag = NA, over_50_percent_fail_window = NA, .rows = 2))
+
+eq75_list <- list(eq75_top, eq75_bottom, eq75_middle)
+
+### test on data with 100% flags
+geq50_100 <- tibble(flag = "sample_flag", over_50_percent_fail_window = NA, .rows = 12)
+
+### test on 3 hour chunk of synthetic data ----
+
+# 0% data flagged
+test_that("no flags added to 0% flagged data", code = {
+  test <- add_suspect_flag(leq50_0)
+  expect_true(is.data.frame(test))
+  expect_no_match(test$flag, "suspect data")
+})
+
+# 25% data flagged
+test_eq_25 <- function(data) {
+  test_that("no flags added to 25% flagged data", code = {
+    test <- add_suspect_flag(data)
+    expect_true(is.data.frame(test))
+    expect_no_match(test$flag, "suspect data")
+  })
+}
+
+walk(eq25_list, test_eq_25)
+
+# 50% data flagged
+test_eq_50 <- function(data) {
+  test_that("suspect flag added to 50% flagged data", code = {
+    test <- add_suspect_flag(data)
+    expect_true(is.data.frame(test))
+    expect_match(test$flag[12], "suspect data")
+  })
+}
+
+walk(eq50_list, test_eq_50)
+
+# 75% data flagged
+test_eq_75 <- function(data) {
+  test_that("suspect flag added to 75% flagged data", code = {
+    test <- add_suspect_flag(data)
+    expect_true(is.data.frame(test))
+    expect_match(test$flag[12], "suspect data")
+  })
+}
+
+walk(eq75_list, test_eq_75)
+
+# 100% data flagged
+test_that("suspect flag added to 100% flagged data", code = {
+  test <- add_suspect_flag(geq50_100)
+  expect_true(is.data.frame(test))
+  expect_match(test$flag[12], "suspect data")
+})
+
+### create a possible combinations of 3 hour chunks to imitate 6 hour chunks of data that will be flagged together ----
+combo_list <- list(
+  leq50_0 = leq50_0,
+  eq25_alternate = eq25_alternate,
+  eq25_bottom = eq25_bottom,
+  eq25_middle = eq25_middle,
+  eq25_top = eq25_top,
+  eq50_alternate = eq50_alternate,
+  eq50_bottom = eq50_bottom,
+  eq50_middle = eq50_middle,
+  eq50_top = eq50_top,
+  eq75_bottom = eq75_bottom,
+  eq75_middle = eq75_middle,
+  eq75_top = eq75_top,
+  geq50_100 = geq50_100
+)
+
+combo_list_crossings <- cross2(combo_list, combo_list)
+
+combo_list_names <- cross2(names(combo_list), names(combo_list)) %>%
+  map(., function(item) {
+    paste(item[[1]], item[[2]], sep = "...")
+  })
+
+pair_combos <- map(combo_list_crossings, bind_rows)
+
+names(pair_combos) <- combo_list_names
+
+### test all possible combinations to see which combinations fail tests ----
+
+flag_pair_combos <- map(pair_combos, add_suspect_flag)
+
+### pair combos with no suspect data flag added
+no_flag_flag_pair_combos <- keep(flag_pair_combos, ~ !any(str_detect(.x$flag, "suspect data"), na.rm = T))
+
+### pair combos with suspect data flag added
+flagged_flag_pair_combos <- keep(flag_pair_combos, ~ any(str_detect(.x$flag, "suspect data"), na.rm = T))
