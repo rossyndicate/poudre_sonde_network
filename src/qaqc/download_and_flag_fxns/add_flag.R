1e13df1c11530637350d1bd2e4e32ea63eb2bf57
juandlt-csu <juan.delatorre@colostate.edu>
2024-03-28 11:47:22 -0600

    working on documentation

--- src/qaqc/download_and_flag_fxns/add_flag.R
+++ src/qaqc/download_and_flag_fxns/add_flag.R
@@ -1,9 +1,21 @@
-# Add a flag to the `flag` column of a dataframe based on a condition and a description of the flag.
-# If the flag is already present in the flag column it will not add another instance of that flag in the flag column
-#' @param df A dataframe with a `flag` column.
-#' @param condition_arg A logical statement that is evaluated in the context of the dataframe.
-#' @param description_arg A string that is added to the flag column if the condition is true.
-#' @return A dataframe with a `flag` column that has been updated with the flag description provided.
+#' @title Underlying function for flagging data.
+#'
+#' @description
+#' This function adds a flag to the `flag` column of a given data frame based on
+#' specified conditions for each row. The name of the flag is provided
+#' by the user.
+#'
+#' @param df A data frame with a `flag` column.
+#'
+#' @param condition_arg A logical statement that is evaluated in the context of
+#' the data frame.
+#'
+#' @param description_arg Flag added to the `flag` column.
+#'
+#' @returns
+#' An identical data frame with a `flag` column that has been updated with the
+#' flag description provided.
+#'
 #' @examples
 # add_flag(df = all_data_flagged$`archery-Actual Conductivity`, condition_arg = mean >= 100, description_arg = "exceeds 100")
 # add_flag(df = all_data_flagged$`archery-Actual Conductivity`, condition_arg = mean <= 10, description_arg = "below 10")
@@ -11,7 +23,7 @@
 add_flag <- function(df, condition_arg, description_arg) {
   df <- df %>% mutate(flag = case_when(
     {{condition_arg}} ~ if_else(is.na(flag), paste(description_arg),
-                                ifelse(!str_detect(flag, description_arg), paste(flag, description_arg, sep = ";\n"), flag)),
+                                ifelse(!grepl(description_arg, flag), paste(flag, description_arg, sep = ";\n"), flag)),
     TRUE ~ flag))
   return(df)
 }
