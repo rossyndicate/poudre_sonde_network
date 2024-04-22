3de6a068de91b81369af8bc7a8d863c6a1ab45cf
kathryn-willi <kathryn.willi@colostate.edu>
2024-04-22 10:35:03 -0600

    moved my scratch stuff to a kw folder

---
+++ src/qaqc/download_and_flag_fxns/add_frozen_flag.R
@@ -0,0 +1,19 @@
+add_frozen_flag <- function(df){
+
+  # create a df of temperature for each site
+  temperature <- df %>%
+    data.table::data.table() %>%
+    dplyr::select(DT_round, DT_join, parameter, mean) %>%
+    dplyr::filter(parameter == "Temperature") %>%
+    dplyr::select(DT_join, Temperature = mean)
+
+    # add "temperature" column to df:
+    temperature_checked <- df %>%
+      dplyr::left_join(., temperature, by = "DT_join") %>%
+      # If water temperature is freezing, flag all parameters
+      add_flag(., Temperature <= 0 & !grepl("frozen", flag), "frozen") %>%
+      dplyr::select(-Temperature)
+
+    return(temperature_checked)
+
+}
