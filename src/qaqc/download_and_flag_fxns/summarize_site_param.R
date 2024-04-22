f6b2ab170d117f973ff542630db45fbf3e3f9474
kathryn-willi <kathryn.willi@colostate.edu>
2024-04-22 10:41:22 -0600

    adding package call to functions

--- src/qaqc/download_and_flag_fxns/summarize_site_param.R
+++ src/qaqc/download_and_flag_fxns/summarize_site_param.R
@@ -1,19 +1,11 @@
 #' @title Summarize site parameter data from the API and field notes data frames.
-#'
 #' @description
 #' A short description...
-#'
 #' @param site_arg A site name.
-#'
 #' @param parameter_arg A parameter name.
-#'
 #' @param api_data A dataframe with the munged API data.
-#'
 #' @param notes The munged field notes
-#'
-#' @return
-#' A dataframe with summary statistics for a given site parameter data frame.
-#'
+#' @return A dataframe with summary statistics for a given site parameter data frame.
 #' @examples
 # summarize_site_param(site_arg = "archery", parameter_arg = "Actual Conductivity", api_data = incoming_data_collated_csvs)
 # summarize_site_param(site_arg = "boxelder", parameter_arg = "Temperature", api_data = incoming_data_collated_csvs)
@@ -22,33 +14,33 @@ summarize_site_param <- function(site_arg, parameter_arg, api_data, notes) {
 
   # filter deployment records for the full join
   site_field_notes <- notes %>%
-    filter(site == site_arg)
+    dplyr::filter(site == site_arg)
 
   # filtering the data and generating results
   summary <- tryCatch({
     api_data %>%
       # summarize the data
-      filter(site == site_arg & parameter == parameter_arg) %>%
-      distinct() %>%
-      group_by(DT_round) %>%
-      summarize(mean = as.numeric(mean(value, na.rm = T)),
-                diff = abs(min(value, na.rm = T) - max(value, na.rm = T)),
-                n_obs = n()) %>%
-      ungroup() %>%
-      arrange(DT_round) %>%
+      dplyr::filter(site == site_arg & parameter == parameter_arg) %>%
+      dplyr::distinct() %>%
+      dplyr::group_by(DT_round) %>%
+      dplyr::summarize(mean = as.numeric(mean(value, na.rm = T)),
+                       diff = abs(min(value, na.rm = T) - max(value, na.rm = T)),
+                       n_obs = n()) %>%
+      dplyr::ungroup() %>%
+      dplyr::arrange(DT_round) %>%
       # pad the dataset so that all 15-min timestamps are present
-      pad(by = "DT_round", interval = "15 min") %>%
+      padr::pad(by = "DT_round", interval = "15 min") %>%
       # join the field notes
-      mutate(DT_join = as.character(DT_round),
-             site = site_arg,
-             parameter = parameter_arg,
-             flag = NA) %>% # maybe we don't want to do this here
-      left_join(filter(dplyr::select(site_field_notes, sonde_employed,
-                                     last_site_visit, DT_join, site,
-                                     visit_comments, sensor_malfunction,
-                                     cals_performed)),
-                by = c('DT_join', 'site')) %>%
-      mutate(DT_round = as_datetime(DT_join, tz = "MST"))
+      dplyr::mutate(DT_join = as.character(DT_round),
+                    site = site_arg,
+                    parameter = parameter_arg,
+                    flag = NA) %>% # maybe we don't want to do this here
+      dplyr::left_join(dplyr::filter(dplyr::select(site_field_notes, sonde_employed,
+                                                   last_site_visit, DT_join, site,
+                                                   visit_comments, sensor_malfunction,
+                                                   cals_performed)),
+                       by = c('DT_join', 'site')) %>%
+      dplyr::mutate(DT_round = lubridate::as_datetime(DT_join, tz = "MST"))
   },
 
   error = function(err) {
