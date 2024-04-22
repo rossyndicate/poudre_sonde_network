ccbfd944e6af0f00f6479b90d70e6bd20575c82b
kathryn-willi <kathryn.willi@colostate.edu>
2024-04-22 10:38:41 -0600

    NEW intrasensor tests

--- src/qaqc/download_and_flag_fxns/add_frozen_flag.R
+++ src/qaqc/download_and_flag_fxns/add_frozen_flag.R
@@ -12,6 +12,7 @@ add_frozen_flag <- function(df){
       dplyr::left_join(., temperature, by = "DT_join") %>%
       # If water temperature is freezing, flag all parameters
       add_flag(., Temperature <= 0 & !grepl("frozen", flag), "frozen") %>%
+      # remove the temp column so df is identical in structure to OG df
       dplyr::select(-Temperature)
 
     return(temperature_checked)
