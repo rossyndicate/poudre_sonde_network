13f17eef4956c7c7c4510ef172c9cd526cd252ba
SamStruthers <sam.struthers2000@gmail.com>
2024-05-01 07:37:52 -0600

    made the log check non case dependant and fixed extra answers in mwater for log type

--- src/mWater_collate/files_missing.R
+++ src/mWater_collate/files_missing.R
@@ -26,8 +26,11 @@ files_missing <- function(){
   field_season <- year(Sys.Date())
 
   #grab cal reports from folder
-  cal_reports_simple <- str_extract(list.files(path = "data/calibration_reports/"), ".*_\\d{8}" )
-  logs_simple <- str_extract(list.files(path = paste0("data/sensor_data/", field_season), recursive = TRUE), "\\w+_\\d{8}_(vulink|troll)")
+  cal_reports_simple <- str_extract(list.files(path = "data/calibration_reports/"), ".*_\\d{8}" )%>%tolower()
+  logs_simple <- str_extract(list.files(path = paste0("data/sensor_data/", field_season), recursive = TRUE), "\\w+_\\d{8}_(vulink|troll)")%>%tolower()
+
+
+
 
   #grab sensor notes that have logs or cal reports that should be  downloaded
   sensor_files <- all_notes_cleaned%>%
@@ -36,16 +39,26 @@ files_missing <- function(){
     filter(cal_report_collected|log_downloaded)%>%
     select(site, crew, start_DT, cal_report_collected, cals_performed, log_downloaded, log1_type,log1_mmdd,  log2_type, log2_mmdd)%>%
     mutate(
-      #correct names if it is in our upper sites (acronyms)
-      site = ifelse(site %in% upper_sites$site_code, toupper(site), site),
+      #make all site names lower
+      site = tolower(site),
       # Create basis for calibration report name
       # this will be used to check for calibration report in data files and then b
-      cal_report_name = case_when(cal_report_collected == TRUE ~ paste0(tolower(site), "_", format(start_DT, "%Y%m%d")),
+      cal_report_name = case_when(cal_report_collected == TRUE ~ paste0(site, "_", format(start_DT, "%Y%m%d")),
                                   cal_report_collected == NA ~ NA),
       log1_mmdd = case_when(nchar(as.character(log1_mmdd)) == 3 ~ paste0("0",log1_mmdd),
                             TRUE ~ as.character(log1_mmdd)),
-      log1_type = tolower(log1_type),
-      log2_type = tolower(log2_type),
+      log1_type = case_when( grepl("aquatroll", log1_type,ignore.case = TRUE) ~  "troll",
+                             grepl("at", log1_type,ignore.case = TRUE) ~  "troll",
+                             grepl("aqua troll", log1_type,ignore.case = TRUE) ~  "troll",
+                             grepl("vulink", log1_type,ignore.case = TRUE) ~  "vulink",
+                             grepl("vu link", log1_type,ignore.case = TRUE) ~  "vulink",
+                             TRUE ~ tolower(log1_type)),
+      log2_type = case_when( grepl("aquatroll", log2_type,ignore.case = TRUE) ~  "troll",
+                             grepl("at", log2_type,ignore.case = TRUE) ~  "troll",
+                             grepl("aqua troll", log2_type,ignore.case = TRUE) ~  "troll",
+                             grepl("vulink", log2_type,ignore.case = TRUE) ~  "vulink",
+                             grepl("vu link", log2_type,ignore.case = TRUE) ~  "vulink",
+                             TRUE ~ tolower(log2_type)),
       log2_mmdd = case_when(nchar(as.character(log2_mmdd)) == 3 ~ paste0("0",log2_mmdd),
                             TRUE ~ as.character(log2_mmdd)),
       log1_user_error = case_when( is.na(log_downloaded)~ FALSE,
@@ -104,3 +117,4 @@ files_missing <- function(){
   cat("\nFile Check Complete")
 
 }
+
