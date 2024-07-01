277bfd2efc48de1246579da215c0191f049c2d3d
SamStruthers <sam.struthers2000@gmail.com>
2024-07-01 13:56:20 -0600

    adding comments to plotter

--- scratch/misc/raw_virridy_plotter.R
+++ scratch/misc/raw_virridy_plotter.R
@@ -1,27 +1,33 @@
+# This script is a basic shiny app to help users verify Virridy sonde data by looking at additional parameters and sites
+
+#Read the data in before running the app to make it faster!
+#data <- readRDS("data/virridy_verification/all_data_flagged_complete.RDS")
 
 library(shiny)
 library(tidyverse)
 library(plotly)
 library(scales)
-#Read the data in before running the app to make it faster!
-#data <- readRDS("data/virridy_verification/all_data_flagged_complete.RDS")
+
 
 # Define UI
 ui <- fluidPage(
   titlePanel("RAW Data Plotter"),
   sidebarLayout(
     sidebarPanel(
-      #radioButtons("data_group", "Network", choices = list("Virridy", "PWQN"), selected = 1),
+      # Allows user to select sites
       selectInput("selected_site", "Select Site(s)",
                   choices = c("joei", "cbri", "chd", "pfal", "sfm", "lbea", "penn", "pbd", "lincoln", "timberline virridy", "timberline",
                               "prospect virridy", "prospect", "archery virridy", "archery", "boxcreek", "springcreek"),
                   selected = "lincoln", multiple = TRUE),
+      # User to select parameters
       selectInput("selected_param", "Select Parameter(s)",
                   choices = c("Depth", "Temperature", "Specific Conductivity", "DO", "Chl-a Fluorescence", "ORP", "Turbidity", "pH", "FDOM Fluorescence"),
                   selected = c("Depth", "Temperature"), multiple = TRUE),
+      #select the number of columns to facet by
       radioButtons("col_number", "Number of Columns for Facet Wrap:", choices = list("One" = 1, "Two" = 2), selected = 1),
       # selectInput("log_param", "Select Parameter(s) for Log Scale",
       #             choices = NULL, selected = NULL, multiple = TRUE),
+      #select dates
       dateRangeInput("date_range", "Select Date Range", start = "2024-03-15", end = "2024-04-15"),
       actionButton("plot_button", "Plot Data")
     ),
@@ -38,7 +44,7 @@ server <- function(input, output
                   # , session
                    ) {
 
-  # # Update log scale parameter choices based on selected parameters
+  # # Trying to get the user to select which parameters to display in log scale. not working yet
   # observe({
   #   selected_params <- input$selected_param
   #   updateSelectInput(session, "log_param", choices = selected_params)
@@ -51,23 +57,24 @@ server <- function(input, output
     # Read in data before running script (results in much faster plotting)
     #data <- readRDS("~/Documents/fork_yeah/poudre_sonde_network/data/virridy_verification/all_data_flagged_complete.RDS")
     # Filter the data based on user input
+
+    #grab the site and parameter combos the user selected
     site_param_combos <- crossing(site_select = input$selected_site, param = input$selected_param) %>%
       mutate(combo = paste(site_select, param, sep = "-")) %>%
       pull(combo)
 
-    all_select_data <- tibble()
-    for (combo in site_param_combos) {
-      if (combo %in% names(data)) {
-        data_select <- data[[combo]]
-        all_select_data <- bind_rows(all_select_data, data_select)
-      }
-    }
-
+#grab the data for the site and parameter combos the user selected
+    all_select_data <- site_param_combos %>%
+      keep(~ .x %in% names(data)) %>%
+      map_dfr(~ data[[.x]])
+#grab start and end dates
     start_date <- as.POSIXct(input$date_range[1])
     end_date <- as.POSIXct(input$date_range[2])
 
+    #trim to just the dates selected
     trim_select_data <- all_select_data %>%
       filter(DT_round >= start_date & DT_round <= end_date)%>%
+      #convert depth to ft (easier to see changes than meters)
       mutate(mean = case_when(parameter == "Depth"~ mean*3.28084,
                               TRUE ~ mean))
 
@@ -81,7 +88,7 @@ server <- function(input, output
         labs(x = "Date", y = "Value") +
         theme_bw()
 
-      # # Apply log scale transformation if necessary
+      # # trying to Apply log scale to parameters userr selected
       # if (!is.null(input$log_param) && length(input$log_param) > 0) {
       #   p <- p + facet_wrap(~parameter, scales = "free_y", ncol = 1) +
       #     geom_line() +
