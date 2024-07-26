61ab78379f70e478d9e02607ae8583bf0af7ac0b
juandlt-csu <juan.delatorre@colostate.edu>
2024-07-26 11:13:04 -0600

    Fixing relevant sonde pulls

--- src/generate_daily_plot.R
+++ src/generate_daily_plot.R
@@ -5,61 +5,9 @@ generate_daily_plot <- function(plot_data_arg, df_list_arg, site_arg, parameter_
   start_date <- min(plot_data_arg$DT_round)
   end_date <- max(plot_data_arg$DT_round)
 
-  #default is lower network
-  site_vector <- c("tamasag", # rist
-                   "legacy",
-                   "lincoln",
-                   "timberline",
-                   "prospect",
-                   "boxelder", # elc
-                   "archery",
-                   "river bluffs")
-
-  if(network == "virridy"){ # this will be the new default
-    # establish non-tributary sites
-    tributary_sites <- tibble(site = c("joei","cbri","chd","pfal","sfm","pbd",
-                                       "tamasag","legacy", "lincoln","timberline",
-                                       "timberline virridy","prospect",
-                                       "prospect virridy","boxelder","archery",
-                                       "archery virridy","riverbluffs"))
-    # establish order for all the non-tributary sites
-    sites_order <-  c("joei","cbri","chd","pfal","sfm","pbd","tamasag",
-                      "legacy","lincoln","timberline","prospect","boxelder",
-                      "archery","riverbluffs")
-    # establish the order for the tributary sites
-    trib_sites_order <- c("boxcreek", "archery", NA, "springcreek", "prospect",
-                          NA, "penn", "sfm", "lbea")
-  }
-
-  # determining the index for the site of interest.
-  if (site_arg %in% tributary_sites$site) {
-
-    plot_filter <- tributary_sites
-
-    site_parent <- strsplit(site_arg, " ")[[1]][1]
-    site_index <- which(sites_order == site_parent)
-    site_list <- as.vector(na.omit(sites_order[max(1, site_index - 1):min(length(sites_order), site_index + 1)]))
-
-    plot_filter <- plot_filter %>%
-      filter(grepl(paste(site_list, collapse = "|"), site, ignore.case = TRUE),
-             site != site_arg) %>%
-      pull(site)
-
-  } else {
-
-    plot_filter <- tibble(site = c("boxcreek", "archery", "archery virridy",
-                                   "springcreek", "prospect", "prospect virridy",
-                                   "penn", "sfm", "lbea"))
-
-    site_index <- which(trib_sites_order == site_arg)
-    site_list <- as.vector(na.omit(trib_sites_order[max(1, site_index - 1):min(length(trib_sites_order), site_index + 1)]))
-
-    plot_filter <- plot_filter %>%
-      filter(grepl(paste(site_list, collapse = "|"), site, ignore.case = TRUE),
-             site != site_arg) %>%
-      pull(site)
-
-  }
+  # establish which sites are relevant to the site of interest
+  # the order of the sites are hardcoded in relevant_sonde_selector function
+  plot_filter <- relevant_sonde_selector(site_arg = site_arg)
 
   # get the relevant sonde data source
   relevant_sonde_source <- map(plot_filter, ~ {
--- src/generate_initial_weekly_plots.R
+++ src/generate_initial_weekly_plots.R
@@ -8,63 +8,10 @@ generate_initial_weekly_plots <- function(all_df_list, pending_df_list, site_arg
     ungroup()
 
   if (!is.null(site_flag_dates)){
-    # vector of sites in the order that they are in spatially ----
-    # some sites have some funkiness going on (not all of the sites are present in the final plot)
-    #default is lower network
-    sites_order <- c("tamasag", # rist
-                     "legacy",
-                     "lincoln",
-                     "timberline",
-                     "prospect",
-                     "boxelder", # elc
-                     "archery",
-                     "river bluffs")
 
-    if(network == "virridy"){ # this will be the new default
-      # establish non-tributary sites
-      tributary_sites <- tibble(site = c("joei","cbri","chd","pfal","sfm","pbd",
-                                         "tamasag","legacy", "lincoln","timberline",
-                                         "timberline virridy","prospect",
-                                         "prospect virridy","boxelder","archery",
-                                         "archery virridy","riverbluffs"))
-      # establish order for all the non-tributary sites
-      sites_order <-  c("joei","cbri","chd","pfal","sfm","pbd","tamasag",
-                        "legacy","lincoln","timberline","prospect","boxelder",
-                        "archery","riverbluffs")
-      # establish order for all the tributary sites
-      trib_sites_order <- c("boxcreek", "archery", NA, "springcreek", "prospect",
-                            NA, "penn", "sfm", "lbea")
-    }
-
-    # determining the sites relevant to the site of interest.
-    if (site_arg %in% tributary_sites$site) {
-
-      plot_filter <- tributary_sites
-
-      site_parent <- strsplit(site_arg, " ")[[1]][1]
-      site_index <- which(sites_order == site_parent)
-      site_list <- as.vector(na.omit(sites_order[max(1, site_index - 1):min(length(sites_order), site_index + 1)]))
-
-      plot_filter <- plot_filter %>%
-        filter(grepl(paste(site_list, collapse = "|"), site, ignore.case = TRUE),
-               site != site_arg) %>%
-        pull(site)
-
-    } else {
-
-      plot_filter <- tibble(site = c("boxcreek", "archery", "archery virridy",
-                                     "springcreek", "prospect", "prospect virridy",
-                                     "penn", "sfm", "lbea"))
-
-      site_index <- which(trib_sites_order == site_arg)
-      site_list <- as.vector(na.omit(trib_sites_order[max(1, site_index - 1):min(length(trib_sites_order), site_index + 1)]))
-
-      plot_filter <- plot_filter %>%
-        filter(grepl(paste(site_list, collapse = "|"), site, ignore.case = TRUE),
-               site != site_arg) %>%
-        pull(site)
-
-    }
+    # establish which sites are relevant to the site of interest
+    # the order of the sites are hardcoded in relevant_sonde_selector function
+    plot_filter <- relevant_sonde_selector(site_arg = site_arg)
 
     if (nrow(site_flag_dates >0)) {
 
@@ -120,6 +67,7 @@ generate_initial_weekly_plots <- function(all_df_list, pending_df_list, site_arg
             } else {
               return(NULL)  # Return NULL if either data_source or sonde_df is NULL
             }
+
           })
 
           # Remove any NULL results from the list
