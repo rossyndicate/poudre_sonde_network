2f06e17f16e668d88888cbe499b3643e5957d754
kathryn-willi <kathryn.willi@colostate.edu>
2024-08-02 13:22:40 -0400

    add all/fcw option

--- src/network_check.R
+++ src/network_check.R
@@ -6,15 +6,13 @@
 #'
 #' @param df A site-parameter data frame that has gone through the initial flagging
 #' process.
-#' @param network Whether the network check is happening across the Virridy sites or CSU sites.
+#' @param network Whether the network check is happening across all sites or CSU sites.
 #' @return A modified data frame flags that have been altered based on the network check.
 #'
 #' @examples
 #' network_check(df = all_data_flagged$`archery-Actual Conductivity`)
 
 network_check <- function(df, network = "csu") {
-
-  df <- df
 
   # get the site name from the site column in the df of interest
   site_name <- unique(na.omit(df$site))
@@ -23,7 +21,7 @@ network_check <- function(df, network = "csu") {
 
   # vector of sites in the order that they are in spatially
   # some sites have some funkiness going on
-
+if(network  %in% c("csu", "CSU", "fcw", "FCW")){
   sites_order <- c("tamasag", # rist
                    "legacy",
                    "lincoln",
@@ -42,7 +40,7 @@ network_check <- function(df, network = "csu") {
               ifelse(site_name == "archery", 17,
               ifelse(site_name == "river bluffs", 17, NA))))))))
 
-  if(network %in% c("virridy", "Virridy")){
+} else if(network %in% c("all", "All")){
 
     sites_order <-  c("joei",
                       "cbri",
@@ -114,61 +112,60 @@ network_check <- function(df, network = "csu") {
   # Generating df name to pull from df_list list
   site_param <- paste0(site_name, "-", parameter_name)
 
-  prev_site_df <- tibble(DT_round = NA)
-  next_site_df <- tibble(DT_round = NA)
+  prev_site_df <- tibble::tibble(DT_round = NA)
+  next_site_df <- tibble::tibble(DT_round = NA)
 
   tryCatch({
     previous_site <- paste0(sites_order[site_index-1],"-",parameter_name)
-    prev_site_df <- intersensor_checks[[previous_site]] %>%
-      select(DT_round, site_up = site, flag_up = flag) %>%
-      data.table()},
+    prev_site_df <- intersensor_flags[[previous_site]] %>%
+      dplyr::select(DT_round, site_up = site, flag_up = flag) %>%
+      data.table::data.table()},
     error = function(err) {
-      cat("No upstream site.\n")})
+      cat(paste0(site_name," has no upstream site with ", parameter_name, ".\n"))})
 
   tryCatch({
     next_site <- paste0(sites_order[site_index+1],"-",parameter_name)
-    next_site_df <- intersensor_checks[[next_site]] %>%
-      select(DT_round, site_down = site, flag_down = flag) %>%
-      data.table()},
+    next_site_df <- intersensor_flags[[next_site]] %>%
+      dplyr::select(DT_round, site_down = site, flag_down = flag) %>%
+      data.table::data.table()},
     error = function(err) {
-      cat("No downstream site.\n")})
+      cat(paste0(site_name, " has no downstream site with ", parameter_name, ".\n"))})
 
 
   join <- df %>%
-    left_join(., prev_site_df, by = "DT_round") %>%
-    left_join(., next_site_df, by = "DT_round")
+    dplyr::left_join(., prev_site_df, by = "DT_round") %>%
+    dplyr::left_join(., next_site_df, by = "DT_round")
 
   if(!("flag_down" %in% colnames(join))) {join$flag_down <- NA}
   if(!("flag_up" %in% colnames(join))) {join$flag_up <- NA}
   if(!("site_down" %in% colnames(join))) {join$site_down <- NA}
   if(!("site_up" %in% colnames(join))) {join$site_up <- NA}
 
-
   # Define a function to check if a given 2-hour window has any instances of the same word
   check_2_hour_window_fail <- function(x) {
     sum(x) >= 1
   }
 
+  # Function to add a column if it doesn't already exist
+  add_column_if_not_exists <- function(df, column_name, default_value = NA) {
+    if (!column_name %in% colnames(df)) {
+      df <- df %>% dplyr::mutate(!!sym(column_name) := default_value)
+    }
+    return(df)
+  }
+
   df_test <- join %>%
     # No upstream/downstream flag = 0
-    mutate(flag_binary = ifelse(
+    dplyr::mutate(flag_binary = ifelse(
       (is.na(flag_up) | grepl("drift|DO interference|repeat|sonde not employed|frozen|unsubmerged|missing data|site visit|sv window|sensor malfunction|sonde burial|sensor biofouling|improper level cal|sonde moved", flag_up)) &
         (is.na(flag_down) | grepl("drift|DO interference|repeat|sonde not employed|frozen|unsubmerged|missing data|site visit|sv window|sensor malfunction|sonde burial|sensor biofouling|improper level cal|sonde moved", flag_down)), 0, 1)) %>%
-    mutate(overlapping_flag = zoo::rollapply(flag_binary, width = width_fun, FUN = check_2_hour_window_fail, fill = NA, align = "center")) %>%
+    dplyr::mutate(overlapping_flag = zoo::rollapply(flag_binary, width = width_fun, FUN = check_2_hour_window_fail, fill = NA, align = "center")) %>%
+    add_column_if_not_exists(column_name = "auto_cleaned_flag") %>%
     # If there is a flag (flags associated with spikes in concentration or funkiness like that), and there is also a flag up/downstream at the same time (2 hour window) it is likely a real
     # WQ event and should therefore not be considered "poor" data:
-    mutate(auto_cleaned_flag = ifelse(!is.na(flag) & !grepl("drift|DO interference|repeat|sonde not employed|frozen|unsubmerged|missing data|site visit|sv window|sensor malfunction|sonde burial|sensor biofouling|improper level cal|sonde moved", flag) & overlapping_flag == TRUE, NA, flag)) %>%
-    select(-c(flag_up, flag_down, site_up, site_down, flag_binary, overlapping_flag))
+    dplyr::mutate(auto_cleaned_flag = ifelse(!is.na(flag) & !grepl("drift|DO interference|repeat|sonde not employed|frozen|unsubmerged|missing data|site visit|sv window|sensor malfunction|sonde burial|sensor biofouling|improper level cal|sonde moved", flag) & overlapping_flag == TRUE, NA, flag)) %>%
+    dplyr::select(-c(flag_up, flag_down, site_up, site_down, flag_binary, overlapping_flag))
 
-  # df_test <- join %>%
-  #   mutate(flag_binary = ifelse(#grepl("slope|suspect", flag) &
-  #     (is.na(flag_up) | grepl("seasonal range|repeat|sonde not employed|missing data|site visit|sv window", flag_up)) &
-  #       (is.na(flag_down) | grepl("seasonal range|repeat|sonde not employed|missing data|site visit|sv window", flag_down)), 0, 1)) %>%
-  #   #arrange(timestamp) %>%
-  #   mutate(overlapping_flag = zoo::rollapply(flag_binary, width = width_fun, FUN = check_2_hour_window_fail, fill = NA, align = "center")) %>%
-  #   mutate(cleaner_flag = ifelse(!is.na(flag) & !grepl("seasonal range|repeat|sonde not employed|missing data|site visit|sv window", flag) & overlapping_flag == TRUE, NA, flag)) %>%
-  #   select(-c(flag_up, flag_down, site_up, site_down, flag_binary, overlapping_flag))
-
-  return(df_test)
+    return(df_test)
 
 }
